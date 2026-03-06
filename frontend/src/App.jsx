508c0634f1f8585290c739443043a2822c4d4dd6
Rakshay123 <146321969+Rakshay123@users.noreply.github.com>
2026-03-06 16:07:46 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   const getVideo = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/mood", {
+      const res = await axios.post("https://mood-recomendation.onrender.com", {
         text: text
       });
 
